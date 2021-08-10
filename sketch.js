a357074602d2ca81372d877a87002cea0f505234
gaurizade <70657468+gaurizade@users.noreply.github.com>
2021-08-10 17:58:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var a= prompt("Enter the first number");
+var b= prompt("Enter the second number");
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+}
